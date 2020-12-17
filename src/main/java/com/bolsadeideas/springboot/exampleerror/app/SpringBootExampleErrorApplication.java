bc79aa0031e8faf4431178831eb03a69b85c0162
jorge <jorgelozano228@gmail.com>
2020-12-17 16:37:34 +0100

    example control errors

---
+++ src/main/java/com/bolsadeideas/springboot/exampleerror/app/SpringBootExampleErrorApplication.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.exampleerror.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExampleErrorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExampleErrorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bolsadeideas/springboot/exampleerror/app/SpringBootExampleErrorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.exampleerror.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExampleErrorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
